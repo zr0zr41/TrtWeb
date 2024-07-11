5e584db900136e9aada3dcdcac004ce459fdb21e
zr0zr41 <zr0zr41@gmail.com>
2024-07-11 05:59:23 +0300

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,4 +3,30 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 require('dotenv').config();
 
-const login = async (req
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.getUserByUsername(username);
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ token, user });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+module.exports = {
+    login
+};
+

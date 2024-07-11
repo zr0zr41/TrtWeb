d2170bc3c7a937a6a0e52ce1057075096d47cef9
zr0zr41 <zr0zr41@gmail.com>
2024-07-11 05:59:53 +0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,24 @@
-"exports.getUser = (req, res) => {\n    // Get user details\n};" 
+const User = require('../models/User');
+
+const register = async (req, res) => {
+    try {
+        const newUser = await User.createUser(req.body);
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: 'Error creating user' });
+    }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(400).json({ message: 'Error fetching users' });
+    }
+};
+
+module.exports = {
+    register,
+    getAllUsers
+};

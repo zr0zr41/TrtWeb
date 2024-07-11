f727db81185a2937f33eb4e75305b539cabb5144
zr0zr41 <zr0zr41@gmail.com>
2024-07-11 08:04:20 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,13 +2,14 @@
 
 require('dotenv').config();
 const tracer = require('dd-trace').init({
-    env: 'production',
-    service: 'webtrt',
-    version: '1.0.0',
+    env: process.env.DD_ENV || 'production',
+    service: process.env.DD_SERVICE || 'trtweb',
+    version: process.env.DD_VERSION || '1.0.0',
     url: `http://${process.env.DD_AGENT_HOST || 'localhost'}:8126`,
-    hostname: 'localhost',
+    hostname: process.env.DD_AGENT_HOST || 'localhost',
     port: 8126
 });
+
 
 const express = require('express');
 const app = express();
